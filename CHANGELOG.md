4f9b128f770ac2084a7d1d8767c99ee4bf392bd2
Shengyou Fan <shengyoufan@gmail.com>
2014-03-02 18:17:51 +0800

    add more completion features
    - add suite name completion
    - add options completion

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Changelog
 
+## 0.0.2 - 2014-03-02
+- add suite name completion
+- add options completion
+
 ## 0.0.1 - 2014-02-13
 - codecept alias and command completion
--- codeception.plugin.zsh
+++ codeception.plugin.zsh
@@ -8,12 +8,32 @@ _codeception_get_command_list () {
 }
 
 _codeception () {
-    if [ -f vendor/bin/codecept ]; then
-        compadd `_codeception_get_command_list`
-    fi
-}
+  local curcontext="$curcontext" state line
+  typeset -A opt_args
 
-# Functions
+  _arguments \
+    '1: :->commands'\
+    '2: :->args'\
+    '*: :->opts'
+
+  if [ -f vendor/bin/codecept ]; then
+    case $state in
+      	commands)
+        	compadd `_codeception_get_command_list`
+        	;;
+        args)
+			case $words[2] in
+				generate:cept|generate:cest|generate:pageobject|generate:phpunit|generate:scenarios|generate:stepobject|generate:suite|generate:test)
+					compadd functional acceptance unit
+					;;
+			esac
+			;;
+      	*)
+			compadd `vendor/bin/codecept $words[2] -h --no-ansi | sed "1,/Options:/d" | sed '/^$/d' | awk '{ print $1 }'`
+			;;
+    esac
+  fi
+}
 
 # Completion setup
 compdef _codeception vendor/bin/codecept
